ddbfb32385aeb9125fd91dfbdd9c3704b8ba7c98
Seol So <cattynip.cattynip@gmail.com>
2023-04-16 21:25:36 -0700

    feat: configure the website

--- components/layouts/RellyShipLayout.tsx
+++ components/layouts/RellyShipLayout.tsx
@@ -1,11 +1,19 @@
+import Head from "next/head";
+
 interface IRellyShipLayoutProps {
   children: React.ReactNode;
 }
 
 const RellyShipLayout = ({ children }: IRellyShipLayoutProps) => {
   return (
-    <div className="dark:mix-blend-darken dark:text-zinc-50 mix-blend-lighten text-black">
-      {children}
+    <div>
+      <Head>
+        <link href="/logo.png" rel="icon" />
+        <title>RellyShip</title>
+      </Head>
+      <main className="dark:mix-blend-darken dark:text-zinc-50 mix-blend-lighten text-black">
+        {children}
+      </main>
     </div>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,23 @@ function RellyShipDocument() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="description"
+          content="RellyShip is a place where you can ask a question to someone, make a vote publicly to users, and even offer a popular celebrity to your program officially."
+        />
+        <meta name="author" content="Seol SO" />
+        <meta name="author" content="cattynip" />
+        <meta name="twitter:title" content="RellyShip" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@cattynip_" />
+        <meta name="twitter:creator" content="@cattynip_" />
+        <meta name="twitter:image" content="/cover-logo.png" />
+        <meta property="og:site_name" content="RellyShip" />
+        <meta name="og:title" content="RellyShip" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/logo-long.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="/logo.png" rel="icon" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
           rel="stylesheet"
