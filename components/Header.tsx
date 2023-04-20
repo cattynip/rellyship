72226d6385d4c456c09bad9fa5e63191d5e16f15
Seol So <cattynip.cattynip@gmail.com>
2023-04-19 19:44:14 -0700

    refactor(component): make the header component responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,38 +37,42 @@ const Header = ({ isUserIn }: IHeaderProps) => {
   const onSearchSubmit: SubmitHandler<ISearchForm> = data => console.log(data);
 
   return (
-    <nav className="w-full m-0 p-0 backdrop-blur-md bg-gradient-to-b from-black to-transparent absolute top-0 left-0 z-10 flex items-center justify-between px-24 border-b border-b-gray-700">
-      <div className="flex items-center">
-        <HeaderElement linkTo={"/"}>
-          <Image src={Logo} alt="RellyShipLogo" width={30} height={30} />
-        </HeaderElement>
-        <HeaderElement>
-          <form onSubmit={handleSubmit(onSearchSubmit)} className="relative">
-            <RellyShipInput
-              placeholder="Search..."
-              extraClassName="border-gray-600 w-full"
-              id="search"
-              formRegister={register("query", {
-                required: true
-              })}
-            />
-            <label
-              className="absolute top-0 right-1.5 h-full flex items-center justify-center"
-              htmlFor="search"
-            >
-              <FontAwesomeIcon
-                icon={"magnifying-glass"}
-                size="xs"
-                className="border border-gray-600 p-1 rounded-md bg-black"
+    <nav className="w-full m-0 p-0 backdrop-blur-md bg-gradient-to-b from-black to-transparent absolute top-0 left-0 z-10 flex items-center justify-around px-24 border-b border-b-gray-700 lg:justify-between">
+      <div className="flex items-center justify-around">
+        <div>
+          <HeaderElement linkTo={"/"}>
+            <Image src={Logo} alt="RellyShipLogo" width={30} height={30} />
+          </HeaderElement>
+        </div>
+        <div className="hidden items-center md:flex">
+          <HeaderElement extraClassName="hidden lg:flex">
+            <form onSubmit={handleSubmit(onSearchSubmit)} className="relative">
+              <RellyShipInput
+                placeholder="Search..."
+                extraClassName="border-gray-600 w-full"
+                id="search"
+                formRegister={register("query", {
+                  required: true
+                })}
               />
-            </label>
-          </form>
-        </HeaderElement>
-        <HeaderElement linkTo={"/asks/ask"} text="Ask" />
-        <HeaderElement linkTo={"/public-votes/open"} text="Public Vote" />
-        <HeaderElement linkTo={"/offers/offer"} text="Offer" />
+              <label
+                className="absolute top-0 right-1.5 h-full flex items-center justify-center"
+                htmlFor="search"
+              >
+                <FontAwesomeIcon
+                  icon={"magnifying-glass"}
+                  size="xs"
+                  className="border border-gray-600 p-1 rounded-md bg-black"
+                />
+              </label>
+            </form>
+          </HeaderElement>
+          <HeaderElement linkTo={"/asks/ask"} text="Ask" />
+          <HeaderElement linkTo={"/public-votes/open"} text="Public Vote" />
+          <HeaderElement linkTo={"/offers/offer"} text="Offer" />
+        </div>
       </div>
-      <div>
+      <div className="hidden md:block">
         {isUserIn ? (
           <div className="flex items-center">
             <HeaderElement linkTo={"/notifications"}>
