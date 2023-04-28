11fa13e2267dabf799d9e0e9c7a23031905b7b71
Seol So <cattynip.cattynip@gmail.com>
2023-04-28 00:12:59 -0700

    fix(component): fix bugs on tag searcher component

--- components/TagSearcher.tsx
+++ components/TagSearcher.tsx
@@ -4,6 +4,8 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import dynamic from "next/dynamic";
 import RellyShipDescription from "./RellyShipComponents/RellyShipDescription";
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 library.add(faXmark);
 
@@ -26,9 +28,14 @@ const TagsSearcher = ({ fallback }: ITagsSearcherProps) => {
     const targetValue = event.target.value;
 
     if (targetValue.endsWith(" ")) {
-      // TODO: Add the checker(regex) for the special letter, such as !, @, #, $, %, ^, &, *, (, ).
+      // TODO: Add the checker(regex) for the special letter, such as !, @, #, $, %, ^, &, *, (, ), ;, :, ., ,, ', ", `, ~
 
       const currentValue = targetValue.trim();
+
+      if (currentValue === "") {
+        event.target.value = "";
+        return;
+      }
 
       const isSame = tags.find(tagValue => {
         if (currentValue.toString() === tagValue.toString()) {
@@ -40,32 +47,41 @@ const TagsSearcher = ({ fallback }: ITagsSearcherProps) => {
 
       if (currentValue.includes("-") || currentValue.includes("@") || isSame) {
         event.target.value = "";
+        return;
       } else {
         setTags(prev => [...prev, currentValue.trim()]);
         event.target.value = "";
+        return;
       }
     }
   };
 
-  const onXMarkCilck = (event: MouseEvent<SVGSVGElement>) => {
+  const onXMarkCilck = async (event: MouseEvent<SVGSVGElement>) => {
     const currentValue =
       event.currentTarget.parentElement?.children[0].children[1].textContent;
 
     setTags(prev =>
-      prev.splice(prev.indexOf(currentValue ? currentValue : ""), 1)
+      prev.filter(tag => {
+        if (tag === currentValue) {
+          return false;
+        }
+        return true;
+      })
     );
   };
 
   return (
-    <div className="border border-gray-500 transition-colors hover:border-white rounded-md pt-3 flex flex-col">
+    <div className="border border-gray-700 hover:border-white rounded-md pt-3 flex flex-col transition-colors">
       <ul className="block px-3 pb-3">
-        {tags.length !== 0 ? (
-          tags.map((tag, tagIdx) => (
-            <TagComponent content={tag} key={tagIdx} onClick={onXMarkCilck} />
-          ))
-        ) : (
-          <RellyShipDescription description="Type the name of the tag and press space bar..." />
-        )}
+        <AnimatePresence mode="sync">
+          {tags.length !== 0 ? (
+            tags.map((tag, tagIdx) => (
+              <TagComponent content={tag} key={tagIdx} onClick={onXMarkCilck} />
+            ))
+          ) : (
+            <RellyShipDescription description="Type the name of the tag and press space bar..." />
+          )}
+        </AnimatePresence>
       </ul>
       <div>
         <RellyShipInput
@@ -88,17 +104,29 @@ const TagComponent = ({
   ...props
 }: ITagComponentProps & HTMLAttributes<HTMLOrSVGElement>) => {
   return (
-    <li className="bg-blue-700 text-blue-300 w-min px-2 py-1 rounded-full flex items-center justify-start space-x-1 m-1 float-left">
-      <div>
-        <span>#</span>
-        <span>{content}</span>
-      </div>
-      <FontAwesomeIcon
-        icon={"xmark"}
-        className="bg-blue-950 py-0.5 px-1 rounded-full cursor-pointer"
-        {...props}
-      />
-    </li>
+    <div>
+      <motion.li
+        className="bg-blue-700 text-blue-300 w-min px-2 py-1 rounded-full flex items-center justify-start space-x-1 m-1 float-left"
+        initial={{
+          y: -20,
+          opacity: 0
+        }}
+        animate={{
+          y: 0,
+          opacity: 1
+        }}
+      >
+        <div>
+          <span>#</span>
+          <span>{content}</span>
+        </div>
+        <FontAwesomeIcon
+          icon={"xmark"}
+          className="bg-blue-950 py-0.5 px-1 rounded-full cursor-pointer"
+          {...props}
+        />
+      </motion.li>
+    </div>
   );
 };
 
